538c07f51f0c2cd7015c87c26a2ba66207867584
hirosaki217 <hirosaki217@gmail.com>
2023-03-28 14:29:01 +0700

    check overload connect

--- src/helpers/check.connect.js
+++ src/helpers/check.connect.js
@@ -1,6 +1,11 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const os = require('os');
+const process = require('process');
+const _SECONDS = 5000;
+
+// count connections
 const countConnect = () => {
     const numConnection = mongoose.connections.length;
     if (numConnection === 0) {
@@ -10,6 +15,24 @@ const countConnect = () => {
     }
 };
 
+// check overload connect
+
+const checkOverloadConnect = () => {
+    setInterval(() => {
+        const numConnection = mongoose.connections.length;
+        const numCores = os.cpus().length;
+        const memoryUsage = process.memoryUsage().rss;
+        // EXAMPLE: maximun number of connections based on number of cores
+        const maxConnections = numCores * 5;
+        console.log(`Active connections: ${numConnections}`);
+        console.log(`Memory usage: ${memoryUsage / 1024 / 1024} MB`);
+        if (numConnection > maxConnections) {
+            console.log(`Connection overload detected`);
+        }
+    }, _SECONDS);
+};
+
 module.exports = {
     countConnect,
+    checkOverloadConnect,
 };
