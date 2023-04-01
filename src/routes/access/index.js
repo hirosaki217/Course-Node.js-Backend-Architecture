4e652d124ab1d65d2136a4a343313c5253ad56ff
hirosaki217 <hirosaki217@gmail.com>
2023-04-01 15:52:11 +0700

    handler error

--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -2,10 +2,11 @@
 
 const express = require('express');
 const accessControler = require('../../controllers/access.controler');
+const { asyncHandler } = require('../../auth/checkAuth');
 
 const router = express.Router();
 // sigup
 
-router.post('/shop/signup', accessControler.signUp);
+router.post('/shop/signup', asyncHandler(accessControler.signUp));
 
 module.exports = router;
--- src/services/apikey.service.js
+++ src/services/apikey.service.js
@@ -1,8 +1,13 @@
 'use strict';
 
 const apikeyModel = require('../models/apikey.model');
-
+const crypto = require('crypto');
 const findById = async (key) => {
+    // const apiKey = crypto.randomBytes(64).toString('hex');
+    // await apikeyModel.create({
+    //     key: apiKey,
+    //     permissions: ['0000'],
+    // });
     const objKey = await apikeyModel.findOne({ key, status: true }).lean();
 
     return objKey;
