0c466757051fc8815bd1345fbb15399fc8079507
hirosaki217 <hirosaki217@gmail.com>
2023-04-01 20:04:38 +0700

    handler success code

--- src/controllers/access.controler.js
+++ src/controllers/access.controler.js
@@ -1,11 +1,16 @@
 'use strict';
 
+const { CREATED } = require('../core/success.respose');
 const AccessService = require('../services/access.service');
 
 class AccessController {
     signUp = async (req, res, next) => {
-        console.log(`[P]::signUp::`, req.body);
-        return res.status(201).json(await AccessService.signUp(req.body));
+        // console.log(`[P]::signUp::`, req.body);
+        new CREATED({
+            message: 'Registered OK!',
+            metadata: await AccessService.signUp(req.body),
+        }).send(res);
+        // return res.status(201).json(await AccessService.signUp(req.body));
     };
 }
 
